2f72aead5f6a7447ff3a96e780dafc86a585314f
pansong <pansong291@sina.com>
2019-05-14 23:28:43 +0800

    Update SNameFactory.java

--- src/proguard/obfuscate/SNameFactory.java
+++ src/proguard/obfuscate/SNameFactory.java
@@ -10,9 +10,9 @@ public class SNameFactory implements NameFactory
    'ۥ','ۦ','ۧ','ۨ','۪','۫','۬','ۭ','​'
   };/**/
   {0x6d8, 0x6d9, 0x6da, 0x6db, 0x6df, 0x6e0, 0x6e1,
-  0x6e2, 0x6e4, 0x6e5, 0x6e6, 0x6e7, 0x6e8, 0x6ea,
-  0x6eb, 0x6ec, 0x6ed, 0x200b
- };
+   0x6e2, 0x6e4, 0x6e5, 0x6e6, 0x6e7, 0x6e8, 0x6ea,
+   0x6eb, 0x6ec, 0x6ed, 0x200b
+  };
  private static final int[] OFFSETS = initOffsets();
  private int index = 0;
  private SNameFactory parent = null;
@@ -80,7 +80,7 @@ public class SNameFactory implements NameFactory
   }
   if(b % a == 0) return true;
   int halfB = b / 2;
-  for(int i = 2; i < halfB; i++)
+  for(int i = 2; i <= halfB; i++)
   {
    if(a % i == 0 && b % i == 0)
     return true;
